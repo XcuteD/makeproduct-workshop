3d11ad99f9db4f3ff8a429b025862a9b5b9a6afa
eXcuteD <xcuted.work@gmail.com>
2022-09-17 11:45:05 +0700

    without adaptation

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $('.reviews__slider').slick({
+        accessibility: true,
+        fade: true,
+        draggable: false,
+        speed: 300,
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+
+        //prevArrow: '<div class="prevArrow-click"> <div class="prevArrow"> </div> </div>',
+        //nextArrow: '<div class="nextArrow-click"> <div class="nextArrow"> </div> </div>'
+        
+    });
+
+});
+
+
